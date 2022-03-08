acb3d39cdf1956cc46caeef0feb82e946031c011
qiansl <qiansl@hua-cloud.com.cn>
2022-03-08 16:54:53 +0800

    删除appid及secret

--- src/test/java/fun/mortnon/wj/test/util/WjTestConst.java
+++ src/test/java/fun/mortnon/wj/test/util/WjTestConst.java
@@ -7,8 +7,8 @@ package fun.mortnon.wj.test.util;
 public class WjTestConst {
 
     /** 腾讯问卷分配的appid */
-    public static final String APP_ID = "tpidFacl1zSq";
+    public static final String APP_ID = "";
 
     /** 腾讯问卷分配的秘钥 */
-    public static final String SECRET = "JHGOJwaZ3ArnaEqRhd3pWWydGFouI28X";
+    public static final String SECRET = "";
 }
